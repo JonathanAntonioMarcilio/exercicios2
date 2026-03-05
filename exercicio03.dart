5c00bd9fe6eb199fd9eba51fe6badd169d88c308
Jonathan Antonio Marcilio <jonathanmarcilio3011@gmail.com>
2026-03-04 22:10:58 -0300

    exercicio 03 iniciado

---
+++ exercicio03.dart
@@ -0,0 +1,20 @@
+Autenticar() async {
+  String token = "token";  
+}
+
+void buscarPerfil(token) async {
+  int userId = 1;
+}
+
+void buscarPedido(userId) async {
+
+  
+
+}
+
+void main(){
+
+
+
+
+}
